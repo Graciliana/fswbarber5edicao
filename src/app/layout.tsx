94c939e5cbaef9d82cf370b08efa61de4be122e6
Graciliana Kascher <eagraciliana@gmail.com>
2024-08-06 19:04:40 -0300

    [chore] setup tailwind and prettierrc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html className="bg-black" lang="en">
       <body className={nunito.className}>{children}</body>
     </html>
   );
