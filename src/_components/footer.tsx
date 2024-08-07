64d27bd5dc7eac771dda4bfb0911dbfd13d34540
Graciliana Kascher <eagraciliana@gmail.com>
2024-08-06 21:03:35 -0300

    [FEAT] add footer initial

---
+++ src/_components/footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className="h-16 bg-secondary-black  flex items-center text-gray-03">
+        <p className="ml-6">© 2023 Copyright FSW Barber</p>
+    </div>
+  )
+}
