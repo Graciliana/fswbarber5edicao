81f440c77a2e0253dfc8a3b06292277ffee27c2c
Graciliana Kascher <eagraciliana@gmail.com>
2024-08-06 23:04:21 -0300

    [FEAT] add date home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../_components/header";
-
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 
 export default function Home() {
@@ -7,8 +8,29 @@ export default function Home() {
     <div className="min-w-96 max-[1440px]">
       <Header />
       <div className="py-6 ">
-         <span className="ml-6 text-xl text-bold text-primary-purple">Olá, </span>
-      <span className="text-sm text-white"> Faça seu Login!</span> 
+         <span className="ml-6 text-xl text-normal text-primary-purple">Olá, </span>
+        <span className="text-xl text-white text-normal "> Faça seu Login!</span> 
+        <p></p>
+      <span className="ml-6 text-sm text-white capitalize">
+        {format(new Date(), "EEEE", {
+          locale: ptBR,
+        })}
+        ,
+      </span>
+      <span className="ml-2 text-sm text-white">
+        {format(new Date(), "dd", {
+          locale: ptBR,
+        })}{" "}
+        de
+      </span>
+      <span className="ml-2 text-sm text-white">
+        {format(new Date(), "MMMM", {
+          locale: ptBR,
+        })}
+      </span>
+      <span className="ml-2 text-sm text-white capitalize">
+        {format(new Date(), "yyyy", {})}
+      </span>
       </div>
      
     </div>
