eed1e43f7d1134cca69f15270691a517193dc519
Graciliana Kascher <eagraciliana@gmail.com>
2024-08-09 00:12:03 -0300

    [FEAT] width adjustment

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className="h-16 bg-secondary-black  flex items-center text-gray-03">
+    <div className="h-16 bg-secondary-black  flex w-full items-center text-gray-03">
         <p className="ml-6 font-normal text-xs">© 2023 Copyright FSW Barber</p>
     </div>
   )
